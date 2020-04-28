d79f346bd0802f731db96ba5fae69fa372da1714
Tim Wargo <tmw65104@uga.edu>
2020-04-28 01:12:17 -0400

    Made the foundation to my chaotic application

---
+++ src/main/java/summer2020/stuff/CancerApp.java
@@ -0,0 +1,28 @@
+package summer2020.stuff;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class CancerApp extends Application {
+
+    private HBox box;
+    private VBox root;
+    private ImageView view;
+    private Button button;
+    private Image discordIcon;
+
+
+    /** {@inheritDoc} **/
+    @Override
+    public void start(Stage stage) {
+
+    } // start
+
+
+} // CancerApp
