8540eb9a3c6f32b7b7456797cbaef49e8edf2f3d
TimWargo <60803886+TimWargo@users.noreply.github.com>
2020-07-19 14:53:31 -0400

    Woked on the login functionality. Yes I know its not good but hey it's my first time trying this.

--- src/main/java/summer2020/stuff/projects/profile/LoginPage.java
+++ src/main/java/summer2020/stuff/projects/profile/LoginPage.java
@@ -1,38 +1,66 @@
 package summer2020.stuff.projects.profile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class LoginPage extends Scene {
 
+    private Button logButton;
+    private Scanner fileScan;
+    private TextField userField;
+    private TextField passField;
+
+
     public LoginPage(Stage stage) {
         super(new Group());
         stage.setTitle("Login");
         Label title = new Label("Log In");
-        TextField userField = new TextField("username");
-        TextField passField = new TextField("password");
-        Region r1 = new Region();
-        r1.setMinHeight(10);
-        Button logButton = new Button("Log In");
-        logButton.setTextAlignment(TextAlignment.CENTER);
-        Label passLink = new Label("Forgot Password?");
-        passLink.setOnMouseClicked(event -> System.out.println("hi"));
+        title.setPadding(new Insets(0, 0, 10, 0));
+        Hyperlink passLink = new Hyperlink("Forgot Password?");
+        passLink.setFont(Font.font("times new roman", 14));
+        passLink.setPadding(new Insets(0, 0, 10, 0));
+        passLink.setOnAction(event -> System.out.println("hi"));
         HBox box = new HBox(passLink);
         box.setAlignment(Pos.CENTER_RIGHT);
-
-        VBox base = new VBox(10, title, r1, userField, passField, box, logButton);
+        Region r2 = new Region();
+        r2.setMinHeight(5);
+        Label signText = new Label("Need an account?");
+        signText.setFont(Font.font("times new roman", 14));
+        Hyperlink signUp = new Hyperlink("Sign up");
+        signUp.setFont(Font.font("times new roman", 14));
+        HBox signBox = new HBox(5, signText, signUp);
+        signBox.setAlignment(Pos.CENTER);
+        Region r3 = new Region();
+        r3.setMinHeight(20);
+        Label version = new Label("Version 1.0.152");
+        HBox box2 = new HBox(version);
+        box2.setAlignment(Pos.CENTER_LEFT);
+        loginFunctionality();
+        VBox baseTop = new VBox(10, title, userField, passField);
+        baseTop.setAlignment(Pos.TOP_CENTER);
+        VBox baseBot = new VBox(10, logButton, r2, signBox, r3, box2);
+        baseBot.setAlignment(Pos.TOP_CENTER);
+        VBox base = new VBox(baseTop, box, baseBot);
         base.setAlignment(Pos.TOP_CENTER);
         base.setPadding(new Insets(50, 20, 5, 20));
         base.setMinWidth(300);
@@ -40,4 +68,36 @@ public class LoginPage extends Scene {
         this.setRoot(base);
         
     } // LoginPage
+
+    /**
+     * Private method that deals with the functionality of the log-in feature.
+     */
+    private void loginFunctionality() {
+        try {
+            fileScan = new Scanner(new File("target/classes/projects/profile/Accounts.txt."));
+        } catch(NullPointerException npe) {
+            System.out.println("Could not find file specified");
+        } catch(FileNotFoundException fnfe) {
+            System.out.println("Could not find file specified");
+        } // try
+        userField = new TextField("username");
+        passField = new TextField("password");
+        logButton = new Button("Log In");
+        logButton.setTextAlignment(TextAlignment.CENTER);
+        logButton.setMaxWidth(300);
+        logButton.setOnAction(event -> {
+            String user = new String(userField.getText());
+            String pass = new String(passField.getText());
+            while(fileScan.hasNext()) {
+                if (fileScan.nextLine().contains("Account #")) {
+                    try {
+                        System.out.println(fileScan.next("username:"));
+                        System.out.println(fileScan.next("password:"));
+                    } catch (NoSuchElementException nsee) {
+                        System.out.println("Does not exist");
+                    } // try
+                } // if
+            } // while
+        });
+    } // loginFunctionality
 } // LoginPage
--- src/main/java/summer2020/stuff/projects/profile/ProfileDriver.java
+++ src/main/java/summer2020/stuff/projects/profile/ProfileDriver.java
@@ -14,9 +14,6 @@ public class ProfileDriver {
             Application.launch(ProfileApp.class, args);
         } catch (UnsupportedOperationException e) {
             System.out.println(e);
-            System.err.println("If this is a DISPLAY problem, then your X server connection");
-            System.err.println("has likely timed out. This can generally be fixed by logging");
-            System.err.println("out and logging back in.");
             System.exit(1);
         } // try
     } // main
